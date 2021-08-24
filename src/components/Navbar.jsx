eb0502eb6cb82c9ad2734df89ab611d1920b5a48
liptonzuma <solomonboateng231@gmail.com>
2021-08-24 02:07:28 +0000

    changed routes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar({items,show}) {
     return (
     <nav className="navbar navbar-expand-lg  navbar-light bg-light fixed-top">
         <div className="container-fluid ">
-          <Link className="navbar-brand logo" to="/">
+          <Link className="navbar-brand logo" to="/sneakerShop/">
             TheSneaker<span className="">Shop</span>
           </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,16 +29,16 @@ function Navbar({items,show}) {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0 center">
               <li className="nav-item">
-                <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                <Link className="nav-link" aria-current="page" to="/sneakerShop/">Home</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/">Link</Link>
+                <Link className="nav-link" to="/sneakerShop/">Link</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/">Link 1</Link>
+                <Link className="nav-link" to="/sneakerShop/">Link 1</Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link " to="/"  >Link 2</Link>
+                <Link className="nav-link " to="/sneakerShop"  >Link 2</Link>
               </li>
             </ul>
             <form className="d-flex">
@@ -48,13 +48,13 @@ function Navbar({items,show}) {
               </button>
             </form>
             <div className="status-bar">
-           <Link to="/notification" className="link"> <IconButton aria-label="bell">
+           <Link to="/sneakerShop/notification" className="link"> <IconButton aria-label="bell">
               <StyledBadge badgeContent={show? 1 :0} color="secondary">
                 <Icon className="fa fa-bell"  />
               </StyledBadge>
             </IconButton>
             </Link>
-            <Link to="/cart" className="link"> <IconButton aria-label="cart">
+            <Link to="/sneakerShop/cart" className="link"> <IconButton aria-label="cart">
               <StyledBadge badgeContent={items >=0?items :0} color="secondary">
                 <ShoppingCartIcon />
               </StyledBadge>
